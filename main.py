75c6c82276ac2b587efb3af25e83edbc9872c75c
wargio <wargio@libero.it>
2019-11-15 16:42:50 +0100

    catch keyboard and handle different port via cmd line

--- main.py
+++ main.py
@@ -4,8 +4,17 @@ from core import Core
 
 def main(argc, argv):
 	core = Core()
-	server = web.Server(core)
-	server.run()
+	port = 8080
+	if argc > 1:
+		if argv[1] == '-h' or argv[1] == '--help':
+			print("usage:  {} <port>".format(argv[0]))
+			sys.exit(0)
+		port = int(argv[1])
+	server = web.Server(core, port)
+	try:
+		server.run()
+	except KeyboardInterrupt:
+		pass
 
 if __name__ == '__main__':
 	main(len(sys.argv), sys.argv)
