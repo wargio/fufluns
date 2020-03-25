afb23b8023a077b5e63580de39540918da3575db
wargio <wargio@libero.it>
2020-03-25 15:29:24 +0100

    Added filesystem strings to detect jailbreak on ios

--- ios/tests/jailbreakcheck.py
+++ ios/tests/jailbreakcheck.py
@@ -13,6 +13,50 @@ queryschemas = {
 	'sileo',
 }
 
+filesystem_strings = {
+	"/Applications/Cydia.app",
+	"/Applications/FakeCarrier.app",
+	"/Applications/Icy.app",
+	"/Applications/IntelliScreen.app",
+	"/Applications/MxTube.app",
+	"/Applications/RockApp.app",
+	"/Applications/SBSettings.app",
+	"/Applications/WinterBoard.app",
+	"/Applications/blackra1n.app",
+	"/Library/MobileSubstrate/DynamicLibraries/LiveClock.plist",
+	"/Library/MobileSubstrate/DynamicLibraries/Veency.plist",
+	"/Library/MobileSubstrate/MobileSubstrate.dylib",
+	"/System/Library/LaunchDaemons/com.ikey.bbot.plist",
+	"/System/Library/LaunchDaemons/com.saurik.Cydia.Startup.plist",
+	"/bin/bash",
+	"/bin/sh",
+	"/etc/apt",
+	"/etc/ssh/sshd_config",
+	"/private/var/lib/apt",
+	"/private/var/lib/cydia",
+	"/private/var/mobile/Library/SBSettings/Themes",
+	"/private/var/stash",
+	"/private/var/tmp/cydia.log",
+	"/usr/bin/sshd",
+	"/usr/libexec/sftp-server",
+	"/usr/libexec/ssh-keysign",
+	"/usr/sbin/sshd",
+	"/var/cache/apt",
+	"/var/lib/apt",
+	"/var/lib/cydia",
+	"/usr/sbin/frida-server",
+	"/usr/bin/cycript",
+	"/usr/local/bin/cycript",
+	"/usr/lib/libcycript.dylib",
+}
+
+def find_fs(offset, string, ctx):
+	ustring = string.strip().upper()
+	for key in filesystem_strings:
+		if key.upper() in ustring:
+			ctx.append(key)
+	return None
+
 def has_permission(ipa, plist, u, permission, description):
 	p = u.dk(plist, permission, description)
 	if len(p) > 0:
@@ -25,20 +69,27 @@ def run_tests(ipa, r2, u, r2h):
 		plist = plistlib.readPlist(tmp[0])
 
 	tmp = u.dk(plist, "LSApplicationQueriesSchemes", [])
-	found = []
+	schema_found = []
+	filesystem_found = []
 
 	if len(tmp) > 0:
 		ipa.logger.notify("Schema[s] found: {}".format('://,'.join(tmp) + '://'))
 
 	for schema in tmp:
 		if schema in queryschemas:
-			found.append(schema + "://")
+			schema_found.append(schema + "://")
+
+	r2h.iterate_strings(r2, find_fs, filesystem_found)
 
 	message = "Jailbreak/Root check is missing"
-	if len(found) > 0:
-		message = "Jailbreak/Root check found via schemas {}".format(", ".join(found))
+	if len(schema_found) and len(filesystem_found):
+		message = "Jailbreak/Root check found via schemas (found {}) and filesystem paths (found {})".format(len(schema_found), len(filesystem_found))
+	elif len(schema_found) > 0:
+		message = "Jailbreak/Root check found via schemas {}".format(", ".join(schema_found))
+	elif len(filesystem_found) > 0:
+		message = "Jailbreak/Root check found via filesystem paths {}".format(", ".join(filesystem_found))
 
-	u.test(ipa, len(found) > 0, message, DESCRIPTION, SEVERITY)
+	u.test(ipa, len(schema_found) > 0, message, DESCRIPTION, SEVERITY)
 
 def name_test():
 	return "Detection Jailbreak/Root"
