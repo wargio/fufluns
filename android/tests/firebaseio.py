bfb1c983773484de1ad80be3813d68bb2ae06d82
wargio <wargio@libero.it>
2020-07-29 17:32:47 +0200

    added firebaseio vulnerable instance detection

---
+++ android/tests/firebaseio.py
@@ -0,0 +1,53 @@
+## fufluns - Copyright 2020 - deroad
+
+import os
+import xml.etree.ElementTree as ET
+import urllib3
+import re
+
+urllib3.disable_warnings()
+
+## CVSS:3.1/AV:N/AC:L/PR:L/UI:N/S:U/C:H/I:L/A:N
+SEVERITY = 7.1
+DESCRIPTION = "Firebase Real-time Databases contains sensitive information on their users, including their email addresses, usernames, passwords, phone numbers, full names, chat messages and location data."
+
+FIREBASE_REGEX = r'https*://(.+?)\.firebaseio.com'
+
+def run_tests(apk, r2s, u, r2h, au):
+	projects = []
+	misconfigured = []
+
+	file = os.path.join("res", "values", "strings.xml")
+	manifest = os.path.join(apk.apktool, file)
+	apk.extra.add_text_file(manifest)
+	root = ET.parse(manifest).getroot()
+	tags = root.findall("string")
+
+	for tag in tags:
+		if tag.text == None:
+			continue
+		project = re.findall(FIREBASE_REGEX, tag.text.strip())
+		if len(project) > 0:
+			projects.extend(project)
+
+	apk.logger.notify("Found {} firebaseio.com projects.".format(len(projects)))
+
+	http = urllib3.PoolManager()
+	for project in projects:
+		url = 'https://{}.firebaseio.com/.json'.format(project)
+		try:
+			resp = http.request('GET', url)
+			if resp.status == 200:
+				misconfigured.append(project)
+			elif resp.status == 401:
+				apk.logger.info("[OK] https://{}.firebaseio.com is secure.".format(project))
+			elif resp.status == 404:
+				apk.logger.notify("[--] https://{}.firebaseio.com/ was not found.".format(project))
+		except urllib3.URLError:
+			apk.logger.error("No network connection. Please check the network connectivity.")
+			return
+	msg = "Misconfigured firebaseio instance {0} over {1} found.".format(len(misconfigured), len(projects))
+	u.test(apk, len(misconfigured) < 1, msg, DESCRIPTION, SEVERITY)
+
+def name_test():
+	return "Misconfigured Firebaseio Instance"
---
+++ ios/tests/firebaseio.py
@@ -0,0 +1,80 @@
+## fufluns - Copyright 2020 - deroad
+
+import glob
+import os
+import plistlib
+import urllib3
+import re
+
+urllib3.disable_warnings()
+
+## CVSS:3.1/AV:N/AC:L/PR:L/UI:N/S:U/C:H/I:L/A:N
+SEVERITY = 7.1
+DESCRIPTION = "Firebase Real-time Databases contains sensitive information on their users, including their email addresses, usernames, passwords, phone numbers, full names, chat messages and location data."
+NO_NETWORK = "No network connection. Please check the network connectivity."
+FIREBASE_REGEX = rb'https*://(.+?)\.firebaseio.com'
+
+def iterate_object(obj):
+	projects = []
+	if isinstance(obj, list):
+		for value in obj:
+			if isinstance(value, (list, dict)):
+				projects.extend(iterate_object(value))
+				continue
+			elif not isinstance(value, str):
+				continue
+			value = value.encode('utf-8')
+			project = re.findall(FIREBASE_REGEX, value)
+			if len(project) > 0:
+				projects.extend(project)
+	else:
+		for key in obj:
+			value = obj[key]
+			if isinstance(value, (list, dict)):
+				projects.extend(iterate_object(value))
+				continue
+			elif not isinstance(value, str):
+				continue
+			value = value.encode('utf-8')
+			project = re.findall(FIREBASE_REGEX, value)
+			if len(project) > 0:
+				projects.extend(project)
+	return projects
+	
+
+def run_tests(ipa, r2, u, r2h):
+	projects = []
+	misconfigured = []
+	plists = glob.glob(os.path.join(ipa.directory, "**", "*.plist"), recursive=True)
+	for file in plists:
+		plist = plistlib.readPlist(file)
+		projects.extend(iterate_object(plist))
+
+	projects = list(filter(lambda x: len(x) > 0, projects))
+
+	ipa.logger.notify("Found {} firebaseio.com projects.".format(len(projects)))
+
+	if len(projects) > 0:
+		http = urllib3.PoolManager()
+		for project in projects:
+			project = project.decode('utf-8')
+			url = 'https://{}.firebaseio.com/.json'.format(project)
+			try:
+				resp = http.request('GET', url)
+				if resp.status == 200:
+					misconfigured.append(project)
+				elif resp.status == 401:
+					ipa.logger.info("[OK] https://{}.firebaseio.com/ is secure.".format(project))
+				elif resp.status == 404:
+					ipa.logger.notify("[--] https://{}.firebaseio.com/ was not found.".format(project))
+			except urllib3.exceptions.NewConnectionError:
+				ipa.logger.error(NO_NETWORK)
+				return
+			except urllib3.exceptions.MaxRetryError:
+				ipa.logger.error(NO_NETWORK)
+				return
+	msg = "Misconfigured firebaseio instance {0} over {1} found.".format(len(misconfigured), len(projects))
+	u.test(ipa, len(misconfigured) < 1, msg, DESCRIPTION, SEVERITY)
+
+def name_test():
+	return "Misconfigured Firebaseio Instance"
